0625c05c7be807d959a220e2594fbd7c60598ae3
Bandar-Zuhair <my.github.websites@gmail.com>
2025-01-02 13:49:04 +0700

    Update maharah-travel.js

--- maharah-travel.js
+++ maharah-travel.js
@@ -468,7 +468,7 @@ function openFullScreenImage(src, text) {
     const whatsappButton = document.createElement('a');
     whatsappButton.className = 'whatsapp_button';
     whatsappButton.innerText = 'إرسال هذا العرض';
-    whatsappButton.href = `https://wa.me/+966532003999?text=طلب%20حجز%20هذا%20العرض:%0Ahttps://mohammed-website.github.io/maharahtravel/${encodeURIComponent(src)}`;
+    whatsappButton.href = `https://wa.me/+966532003999?text=طلب%20حجز%20هذا%20العرض:%0A%0Ahttps://mohammed-website.github.io/maharahtravel/${encodeURIComponent(src)}`;
     fullScreenDiv.appendChild(whatsappButton);
 
     // Close on background click
